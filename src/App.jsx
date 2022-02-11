5137545a1ea91d8ea1e890308ff09be89af2a5ee
TaniaOlivas <taniaolivas24@gmail.com>
2022-02-11 10:10:28 -0500

    Added FoodTable

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import FoodIndex from './components/Food/FoodIndex';
 
 function App() {
-  return <div>{/* Tania Branch */}</div>;
+  return (
+    <div>
+      <FoodIndex />
+    </div>
+  );
 }
 
 export default App;
