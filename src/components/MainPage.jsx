c94227e704ab078e39fce20c48b12c53579464ec
TaniaOlivas <taniaolivas24@gmail.com>
2022-02-16 09:57:05 -0500

    Added background image

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,16 +3,18 @@ import { Route, Routes, Link } from 'react-router-dom';
 import FoodIndex from './Food/FoodIndex';
 import UserIndex from './users/UserIndex';
 import fruitImage from '../assets/fruitphoto.jpg';
+import { Button } from 'reactstrap';
 
 const MainPage = (props) => {
   return (
     <div className="backgroundMain">
       <div>
-        <Link to="/FoodIndex">Log your Food!</Link>{' '}
+        <Button>
+          <Link to="/FoodIndex">Log your Food!</Link>
+        </Button>{' '}
         <Link to="/users">Admin Only</Link>
       </div>
       <div>
-        {/* <img src={ fruitImage } style={{height:'auto',width:'100%'}} /> */}
         <Routes>
           <Route
             exact
