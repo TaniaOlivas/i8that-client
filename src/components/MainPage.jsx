f030539b8aa2bf1ffe515d0b6cba21b8786ff193
heidibeth <93995431+heidibeth@users.noreply.github.com>
2022-02-16 15:50:55 -0500

    logo change

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -11,7 +11,7 @@ const MainPage = (props) => {
       <header className="secondHeader">
         <Navbar style={{paddingTop: 0, marginLeft: "20px"}} >
         <div>
-            <img src={logo} style={{ width: "70%", height: "70%"}} />
+            <img src={logo} style={{ width: "120px", height: "60px", paddingTop: 0, margin: -12}} />
             </div>
           <Nav navbar className='ms-auto' style={{textAlign: 'right'}}>
             <NavItem>
